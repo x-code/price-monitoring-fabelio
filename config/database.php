2901f9d308e5cdc1256a89811e1e233967023512
Rian Firandika <coderteam@yahoo.com>
2019-07-30 13:01:35 +0700

    add procfile

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'price_monitoring'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' => env('DB_DATABASE', 'fabelio_pm'),
+            'username' => env('DB_USERNAME', 'fabelio'),
+            'password' => env('DB_PASSWORD', 'fabelio09876'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
